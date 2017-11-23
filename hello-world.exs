4b135ee8b9523028d598c040c6a787d8c2ee7762
Łukasz Wiatrak <Lukasz.Wiatrak@medius-group.pl>
2017-11-23 19:18:33 +0100

    print example

--- hello-world.exs
+++ hello-world.exs
@@ -1,5 +1,40 @@
-getPair = fn (index) -> index end
+
+example = [
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0]
+]
+
+heights = [
+    2, 2, 3, 1,
+    1, 3, 2, 2,
+    2, 2, 3, 1,
+    1, 3, 2, 2
+]
+
+#def isValid(partialSolution, heights) do
+#   # for n <- [1, 2, 3, 4], do: IO.puts n
+#end
+
+
+# getPair = fn (index) -> index end
+
+defmodule Math do
+  def sum(a, b) do
+    a + b
+  end
+end
+
+Enum.each(example, fn (row) -> Enum.join(row, "\t") |> IO.puts end)
+
+IO.inspect example
+
+IO.puts Math.sum(1,2)
+#
+#isValid.([], 1)
+
 
 
 # sum = fn (a, b) -> a + b end
-IO.puts getPair.(1)
+#IO.puts getPair.(1)
